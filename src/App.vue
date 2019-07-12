e63a1d1e674ea8193906a6d46fc43f99bbdc8ae5
weite122 <949193531@qq.com>
2019-07-12 23:56:08 +0800

    购物车初始结构

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <ul>
+            <li v-for="(good, index) in goods" :key="good.id">{{good.text}}
+                <span>{{good.text}}</span>
+                <span>${{good.price}}</span>
+                <button @click="addCart(index)">添加购物车</button>
+            </li>
+
+
+        </ul>
+        <cart :name="name" :cart="cart"></cart>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Cart from "./components/Cart.vue";
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  export default {
+    name: "app",
+    components: {
+      Cart
+    },
+    data() {
+      return {
+        name: "学习课程",
+        text: "",
+        goods: [
+          {id: 1, text: "c", price: 100},
+          {id: 2, text: "web", price: 80},
+          {id: 3, text: "Python", price: 60},
+          {id: 4, text: "Java", price: 80}
+        ],
+        cart: []
+      };
+    },
+    methods: {
+      addCart(i) {
+        const good = this.goods[i];
+        const ret = this.cart.find(v => v.text == good.text);
+        if (ret) {
+          ret.count += 1;
+        } else {
+          this.cart.push({...good, active: true, count: 1});
+        }
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: "Avenir", Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+
+    ul, li {
+        list-style: none;
+    }
 </style>
---
+++ src/components/Cart.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div>{{name}}购物车</div>
+    <table border="1">
+      <tr>
+        <th>课程名</th>
+        <th>单价</th>
+        <th>数量</th>
+        <th>价格</th>
+      </tr>
+      <tr v-for="c in cart" :key="c.id">
+        <td>{{c.text}}</td>
+        <td>￥{{c.price}}</td>
+        <td>{{c.count}}</td>
+        <td>{{c.count*c.price}}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Cart",
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    cart: {
+      type: Array
+    }
+  },
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
