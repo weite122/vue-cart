82fcd21ec0c873ea11b5f70acfa825a4314f791c
weite122 <949193531@qq.com>
2019-07-13 23:58:58 +0800

    [A]添加点击事件

--- src/components/Cart.vue
+++ src/components/Cart.vue
@@ -1,40 +1,66 @@
 <template>
-  <div>
-    <div>{{name}}购物车</div>
-    <table border="1">
-      <tr>
-        <th>课程名</th>
-        <th>单价</th>
-        <th>数量</th>
-        <th>价格</th>
-      </tr>
-      <tr v-for="c in cart" :key="c.id">
-        <td>{{c.text}}</td>
-        <td>￥{{c.price}}</td>
-        <td>{{c.count}}</td>
-        <td>{{c.count*c.price}}</td>
-      </tr>
-    </table>
-  </div>
+    <div>
+        <div>{{name}}购物车</div>
+        <table border="1">
+            <tr>
+                <th>#</th>
+                <th>课程名</th>
+                <th>单价</th>
+                <th>数量</th>
+                <th>价格</th>
+            </tr>
+            <tr v-for="(c, index) in cart" :key="c.id" :class="{active:c.active}">
+                <td>
+                    <input type="checkbox" v-model="c.active">
+                </td>
+                <td>{{c.text}}</td>
+                <td>￥{{c.price}}</td>
+                <td>
+                    <button @click="minus(index)">-</button>
+                    {{c.count}}
+                    <button @click="add(index)">+</button>
+                </td>
+                <td>{{c.count*c.price}}</td>
+            </tr>
+        </table>
+    </div>
 </template>
 
 <script>
-export default {
-  name: "Cart",
-  props: {
-    name: {
-      type: String,
-      required: true
+  export default {
+    name: "Cart",
+    props: {
+      name: {
+        type: String,
+        required: true
+      },
+      cart: {
+        type: Array
+      }
     },
-    cart: {
-      type: Array
-    }
-  },
-  data() {
-    return {};
-  },
-  methods: {}
-};
+    data() {
+      return {};
+    },
+    methods: {
+      minus(i) {
+        const count = this.cart[i].count
+        if(count > 1) {
+          this.cart[i].count -= 1
+        } else {
+          this.remove(i)
+        }
+      },
+      add(i) {
+        this.cart[i].count +=1
+      },
+      remove(i){
+        if (window.confirm('确定删除?')) {
+          this.cart.splice(i, 1)
+        }
+      }
+    },
+
+  };
 </script>
 
 <style lang="scss" scoped>
